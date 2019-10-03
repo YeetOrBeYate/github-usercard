7ce41c27967fcc00041fe482de6f198f194b388d
Kyle Corbin <kylecorbin26@gmail.com>
2019-10-03 17:14:43 -0400

    finished project, and streth goal

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,25 +1,30 @@
-/* Step 1: using axios, send a GET request to the following URL 
+/* Step 1: using axios, send a GET request to the following URL
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
 
-/* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
+// axios.get('https://api.github.com/users/YeetOrBeYate')
+//   .then((res) => {
+//     console.log(res);
+//   })
+
+/* Step 2: Inspect and study the data coming back, this is YOUR
+   github info! You will need to understand the structure of this
+   data in order to use it to build your component function
 
    Skip to Step 3.
 */
 
-/* Step 4: Pass the data received from Github into your function, 
+/* Step 4: Pass the data received from Github into your function,
            create a new component and add it to the DOM as a child of .cards
 */
 
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
+/* Step 5: Now that you have your own card getting added to the DOM, either
+          follow this link in your browser https://api.github.com/users/<Your github name>/followers
+          , manually find some other users' github handles, or use the list found
           at the bottom of the page. Get at least 5 different Github usernames and add them as
           Individual strings to the friendsArray below.
-          
+
           Using that array, iterate over it, requesting data for each user, creating a new card for each
           user, and adding that card to the DOM.
 */
@@ -35,7 +40,7 @@ const followersArray = [];
     <h3 class="name">{users name}</h3>
     <p class="username">{users user name}</p>
     <p>Location: {users location}</p>
-    <p>Profile:  
+    <p>Profile:
       <a href={address to users github page}>{address to users github page}</a>
     </p>
     <p>Followers: {users followers count}</p>
@@ -46,7 +51,67 @@ const followersArray = [];
 
 */
 
-/* List of LS Instructors Github username's: 
+function makePerson(person){
+  const card = document.createElement('div'),
+        image = document.createElement('img'),
+        cardInfo = document.createElement('div'),
+        name = document.createElement('h3'),
+        userName = document.createElement('p'),
+        location = document.createElement('p'),
+        profile = document.createElement('p'),
+          address = document.createElement('a'),
+        followers = document.createElement('p'),
+        following = document.createElement('p'),
+        bio = document.createElement('p');
+//adding classes
+  card.classList.add('card');
+  cardInfo.classList.add('card-info');
+  name.classList.add('name');
+  userName.classList.add('username');
+//adding info
+  image.src = person.avatar_url;
+  name.innerText = person.name;
+  userName.innerText = person.login;
+  location.innerText = person.location;
+  address.innerText = person.html_url;
+  address.href = person.html_url;
+  followers.innerText = `Followers: ${person.followers}`;
+  following.innerText = `Following: ${person.following}`;
+//appending
+  card.appendChild(image);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(name);
+  cardInfo.appendChild(userName);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+  profile.appendChild(address);
+
+  return card;
+}
+const entry = document.querySelector('.cards');
+
+axios.get('https://api.github.com/users/YeetOrBeYate')
+  .then((res) => {
+    entry.appendChild(makePerson(res.data))
+
+    axios.get(res.data.followers_url)
+    .then((res)=>{
+      res.data.forEach((fr)=>{
+
+          axios.get(`https://api.github.com/users/${fr.login}`)
+            .then((res)=>{
+              entry.appendChild(makePerson(res.data))
+            })
+        })
+    })
+})
+
+
+
+/* List of LS Instructors Github username's:
   tetondan
   dustinmyers
   justsml
